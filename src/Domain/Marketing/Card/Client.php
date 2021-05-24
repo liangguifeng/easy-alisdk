c5c7529afc23a45e22e6d013402a51c70e522447
liangguifeng <1476982312@qq.com>
2021-05-24 15:09:39 +0800

    会员卡模板修改
    Signed-off-by: liangguifeng <1476982312@qq.com>

--- src/Domain/Marketing/Card/Client.php
+++ src/Domain/Marketing/Card/Client.php
@@ -2,6 +2,7 @@
 
 namespace EasyAliSdk\Domain\Marketing\Card;
 
+use EasyAliSdk\Domain\Marketing\Card\Models\AlipayMemberCardTemplateModifySendResponse;
 use Exception;
 use AlibabaCloud\Tea\Tea;
 use AlibabaCloud\Tea\Request;
@@ -187,6 +188,168 @@ class Client
     }
 
     /**
+     * 修改会员卡模板
+     *
+     * @param $requestId
+     * @param $templateId
+     * @param $bizNoSuffixLen
+     * @param $writeOffType
+     * @param null $bizNoPrefix
+     * @param null $cardSpecTag
+     *
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     *
+     * @return AlipayMemberCardTemplateModifySendResponse
+     */
+    public function templateModify(
+        $requestId,
+        $templateId,
+        $writeOffType,
+        array $templateStyleInfo,
+        array $columnInfoList,
+        array $fieldRuleList,
+        array $templateBenefitInfo = [],
+        array $cardActionList = [],
+        array $openCardConf = [],
+        array $shopIds = [],
+        array $pubChannels = [],
+        array $cardLevelConf = [],
+        array $mdcodeNotifyConf = [],
+        $bizNoPrefix = null,
+        $cardSpecTag = null
+    ) {
+        $_runtime = [
+            'ignoreSSL'      => $this->_kernel->getConfig('ignoreSSL'),
+            'httpProxy'      => $this->_kernel->getConfig('httpProxy'),
+            'connectTimeout' => 15000,
+            'readTimeout'    => 15000,
+            'retry'          => [
+                'maxAttempts' => 0,
+            ],
+        ];
+        $_lastRequest   = null;
+        $_lastException = null;
+        $_now           = time();
+        $_retryTimes    = 0;
+        while (Tea::allowRetry(@$_runtime['retry'], $_retryTimes, $_now)) {
+            if ($_retryTimes > 0) {
+                $_backoffTime = Tea::getBackoffTime(@$_runtime['backoff'], $_retryTimes);
+
+                if ($_backoffTime > 0) {
+                    Tea::sleep($_backoffTime);
+                }
+            }
+            $_retryTimes = $_retryTimes + 1;
+
+            try {
+                $_request     = new Request();
+                $systemParams = [
+                    'method'              => 'alipay.marketing.card.template.modify',
+                    'app_id'              => $this->_kernel->getConfig('appId'),
+                    'timestamp'           => $this->_kernel->getTimestamp(),
+                    'format'              => 'json',
+                    'version'             => '1.0',
+                    'alipay_sdk'          => $this->_kernel->getSdkVersion(),
+                    'charset'             => 'UTF-8',
+                    'sign_type'           => $this->_kernel->getConfig('signType'),
+                    'app_cert_sn'         => $this->_kernel->getMerchantCertSN(),
+                    'alipay_root_cert_sn' => $this->_kernel->getAlipayRootCertSN(),
+                ];
+                $bizParams = [
+                    'request_id'          => $requestId,
+                    'template_id'         => $templateId,
+                    'write_off_type'      => $writeOffType,
+                    'template_style_info' => $templateStyleInfo,
+                    'column_info_list'    => $columnInfoList,
+                    'field_rule_list'     => $fieldRuleList,
+                    'biz_no_prefix'       => $bizNoPrefix,
+                    'card_action_list'    => $cardActionList,
+                ];
+
+                if (!empty($templateBenefitInfo)) {
+                    $bizParams['template_benefit_info'] = $templateBenefitInfo;
+                }
+
+                if (!empty($bizNoPrefix)) {
+                    $bizParams['biz_no_prefix'] = $bizNoPrefix;
+                }
+
+                if (!empty($cardActionList)) {
+                    $bizParams['card_action_list'] = $cardActionList;
+                }
+
+                if (!empty($openCardConf)) {
+                    $bizParams['open_card_conf'] = $openCardConf;
+                }
+
+                if (!empty($shopIds)) {
+                    $bizParams['shop_ids'] = $shopIds;
+                }
+
+                if (!empty($pubChannels)) {
+                    $bizParams['pub_channels'] = [
+                        $pubChannels,
+                    ];
+                }
+
+                if (!empty($cardLevelConf)) {
+                    $bizParams['card_level_conf'] = [
+                        $cardLevelConf,
+                    ];
+                }
+
+                if (!empty($mdcodeNotifyConf)) {
+                    $bizParams['mdcode_notify_conf'] = $mdcodeNotifyConf;
+                }
+
+                if (!empty($cardSpecTag)) {
+                    $bizParams['card_spec_tag'] = $cardSpecTag;
+                }
+
+                $textParams         = [];
+                $_request->protocol = $this->_kernel->getConfig('protocol');
+                $_request->method   = 'POST';
+                $_request->pathname = '/gateway.do';
+                $_request->headers  = [
+                    'host'         => $this->_kernel->getConfig('gatewayHost'),
+                    'content-type' => 'application/x-www-form-urlencoded;charset=utf-8',
+                ];
+                $_request->query = $this->_kernel->sortMap(Tea::merge([
+                    'sign' => $this->_kernel->sign($systemParams, $bizParams, $textParams, $this->_kernel->getConfig('merchantPrivateKey')),
+                ], $systemParams, $textParams));
+                $_request->body = $this->_kernel->toUrlEncodedRequestBody($bizParams);
+                $_lastRequest   = $_request;
+                $_response      = Tea::send($_request, $_runtime);
+                $respMap        = $this->_kernel->readAsJson($_response, 'alipay.marketing.card.template.modify');
+
+                if ($this->_kernel->isCertMode()) {
+                    if ($this->_kernel->verify($respMap, $this->_kernel->extractAlipayPublicKey($this->_kernel->getAlipayCertSN($respMap)))) {
+                        return AlipayMemberCardTemplateModifySendResponse::fromMap($this->_kernel->toRespModel($respMap));
+                    }
+                } else {
+                    if ($this->_kernel->verify($respMap, $this->_kernel->getConfig('alipayPublicKey'))) {
+                        return AlipayMemberCardTemplateModifySendResponse::fromMap($this->_kernel->toRespModel($respMap));
+                    }
+                }
+                throw new TeaError([
+                    'message' => '验签失败，请检查支付宝公钥设置是否正确。',
+                ]);
+            } catch (Exception $e) {
+                if (!($e instanceof TeaError)) {
+                    $e = new TeaError([], $e->getMessage(), $e->getCode(), $e);
+                }
+
+                if (Tea::isRetryable($e)) {
+                    $_lastException = $e;
+                    continue;
+                }
+                throw $e;
+            }
+        }
+        throw new TeaUnableRetryError($_lastRequest, $_lastException);
+    }
+
+    /**
      * 会员卡表单模板配置.
      *
      * @param $requestId
@@ -241,8 +404,8 @@ class Client
                     'alipay_root_cert_sn' => $this->_kernel->getAlipayRootCertSN(),
                 ];
                 $bizParams = [
-                    'template_id'          => $templateId,
-                    'fields'               => $fields,
+                    'template_id' => $templateId,
+                    'fields'      => $fields,
                 ];
                 $textParams         = [];
                 $_request->protocol = $this->_kernel->getConfig('protocol');
@@ -445,9 +608,9 @@ class Client
                     'alipay_root_cert_sn' => $this->_kernel->getAlipayRootCertSN(),
                 ];
                 $bizParams = [
-                    'biz_type'                    => $bizType,
-                    'template_id'                 => $templateId,
-                    'request_id'                  => $requestId,
+                    'biz_type'    => $bizType,
+                    'template_id' => $templateId,
+                    'request_id'  => $requestId,
                 ];
 
                 $textParams         = [];
@@ -545,10 +708,10 @@ class Client
                     'alipay_root_cert_sn' => $this->_kernel->getAlipayRootCertSN(),
                 ];
                 $bizParams = [
-                    'out_serial_no'                    => $outSerialNo,
-                    'card_template_id'                 => $cardTemplateId,
-                    'card_user_info'                   => $cardUserInfo,
-                    'card_ext_info'                    => $cardExtInfo,
+                    'out_serial_no'    => $outSerialNo,
+                    'card_template_id' => $cardTemplateId,
+                    'card_user_info'   => $cardUserInfo,
+                    'card_ext_info'    => $cardExtInfo,
                 ];
 
                 if (!empty($memberExtInfo)) {
